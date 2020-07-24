a436cb869897fbe2068e24dc7d9f2f4a5cddbb10
Aditya Ghidora <adityaghidora@gmail.com>
2020-07-24 20:13:56 +0530

    added abbreviations for zsh

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,26 +1,28 @@
-# Aliases
-alias zshcfg="nvim ~/.config/zsh/.zshrc"
-alias zshsrc="source ~/.config/zsh/.zshrc"
-alias sudo="sudo "
-alias gst="git status | more"
-alias ls="ls --color"
-alias ll="ls -l --color"
-alias la="ls -a --color"
-alias lal="ls -al --color"
-alias downloads="rcd /MassStorage/downloads/"
-alias r="ranger"
-alias rcfg="nvim ~/.config/ranger/rc.conf"
-alias vim="nvim"
-alias pac="sudo pacman"
-alias coding="rcd /MassStorage/coding/"
-alias media="rcd /MassStorage/Media/"
-alias vimcfg="nvim ~/.config/nvim/init.vim"
-
 # Sourcing my plugins
 source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/plugins/zsh-abbr/zsh-abbr.zsh
 source ~/.config/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 source ~/.config/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
 source ~/.config/zsh/plugins/zsh-colored-man-pages/colored-man-pages.plugin.zsh
 source ~/.config/zsh/plugins/colorize/colorize.plugin.zsh
+source ~/.local/share/lscolors.sh
+
+# Aliases
+abbr zshcfg="nvim ~/.config/zsh/.zshrc"
+abbr zshsrc="source ~/.config/zsh/.zshrc"
+abbr sudo="sudo "
+abbr gst="git status | more"
+abbr ls="ls --color"
+abbr ll="ls -l --color"
+abbr la="ls -a --color"
+abbr lal="ls -al --color"
+abbr downloads="rcd /MassStorage/downloads/"
+abbr r="ranger"
+abbr rcfg="nvim ~/.config/ranger/rc.conf"
+abbr vim="nvim"
+abbr pac="sudo pacman"
+abbr coding="rcd /MassStorage/coding/"
+abbr media="rcd /MassStorage/Media/"
+abbr vimcfg="nvim ~/.config/nvim/init.vim"
 
 
